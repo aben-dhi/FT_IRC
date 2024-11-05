f46a4aa86853fe735ca50721dd86c0ea3d63e481
rrakman <r3da@protonmail.com>
2024-11-06 00:55:55 +0100

    fixed some issues, need to fix parsing

--- srcs/requestHandling.cpp
+++ srcs/requestHandling.cpp
@@ -17,21 +17,27 @@ void	Server::_ClientRequest(int i)
 	char buffer[6000];
 	int sender_fd = this->_pfds[i].fd;
 	int nbytes = recv(sender_fd, buffer, sizeof(buffer) - 1, 0);
-	buffer[nbytes] = '\0';
-	if (nbytes <= 0)
+	if (nbytes < 0)
 	{
-		if (nbytes == 0)
-			std::cout << "[" << currentDateTime() << "]:" << "socket " << sender_fd << " hung up" << std::endl;
+		if(errno == EWOULDBLOCK  || errno == EAGAIN)
+			return;
 		else
+		{
 			std::cerr << "recv() error:" << strerror(errno) << std::endl;
+			close(sender_fd);
+			_removeFromPoll(i);
+		}
+	}
+	else if (nbytes == 0)
+	{
+		std::cout << "[" << currentDateTime() << "]:" << "socket " << sender_fd << " hung up" << std::endl;
 		close(sender_fd);
 		_removeFromPoll(i);
 	}
 	else
 	{
-		std::string message(buffer, strlen(buffer) - 1);
-		// if (message.back() == '\r')
-		// 	message.erase(message.end());
+		buffer[nbytes] = '\0';
+		std::string message(buffer, nbytes);
 		std::string ret = _parsing(message, this->_pfds[i].fd);
 		if (send(sender_fd, ret.c_str(), ret.length(), 0) == -1)
 			std::cerr << "send() error:" << strerror(errno) << std::endl;
@@ -73,24 +79,28 @@ Request Server::_splitRequest(std::string req)
 				return (request);
 			}
 			request._args.push_back(req.substr(i + 1, req.length() - i));
-			std::string command = request._args[0];
-			if(!command.empty() && command.back() == '\n')
-				command.pop_back();
-			request._command = command;
+			request._command = request._args[0];
 			request._args.erase(request._args.begin());
+			// std::cout<<request._command<<std::endl;
+			// for (size_t i = 0; i < request._args.size(); i++)
+			// 	std::cout<<request._args[i]<<std::endl;
 			return (request);
 		}
 		i++;
 	}
 	if (i && req[j])
 		request._args.push_back(req.substr(j, i - j));
-	std::string command = request._args[0];
-	// if(!command.empty() && command.back() == '\n')
-	// 	command.pop_back();
-	request._command = command;
+	request._command = request._args[0];
 	request._args.erase(request._args.begin());
+	if(!request._args[0].empty() && request._args[0].back() == '\n')
+		request._args[0].pop_back();
+
+
+	// DEBUG
 	std::cout<<request._command<<std::endl;
 	for (size_t i = 0; i < request._args.size(); i++)
 		std::cout<<request._args[i]<<std::endl;
+	
+	
 	return (request);
 }
