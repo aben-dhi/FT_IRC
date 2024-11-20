a7a5feb7fdb7262ba62713bd5185e927e4085166
Amira Ben dhia <aben-dhi@c2r6p6.1337.ma>
2024-11-20 05:39:37 +0100

    removed realloc

--- srcs/pollHandling.cpp
+++ srcs/pollHandling.cpp
@@ -6,24 +6,41 @@
 /*   By: aben-dhi <aben-dhi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/04 23:35:06 by aben-dhi          #+#    #+#             */
-/*   Updated: 2024/09/05 20:46:22 by aben-dhi         ###   ########.fr       */
+/*   Updated: 2024/11/20 05:38:50 by aben-dhi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/Server.hpp"
 
-void	Server::_addToPoll(int fd)
-{
-	if (this->_online_c == this->_max_c)
-	{
-		this->_max_c *= 2;
-		this->_pfds = (struct pollfd *)realloc(this->_pfds, this->_max_c);
-	}
-	this->_pfds[this->_online_c].fd = fd;
-	this->_pfds[this->_online_c].events = POLLIN;
-	this->_clients.insert(std::pair<int, Client*>(fd, new Client(fd)));
-	this->_online_c++;
+// void	Server::_addToPoll(int fd)
+// {
+// 	if (this->_online_c == this->_max_c)
+// 	{
+// 		this->_max_c *= 2;
+// 		this->_pfds = (struct pollfd *)realloc(this->_pfds, this->_max_c);
+// 	}
+// 	this->_pfds[this->_online_c].fd = fd;
+// 	this->_pfds[this->_online_c].events = POLLIN;
+// 	this->_clients.insert(std::pair<int, Client*>(fd, new Client(fd)));
+// 	this->_online_c++;
+// }
+
+void Server::_addToPoll(int fd) {
+    if (this->_online_c == this->_max_c)
+    {
+        this->_max_c *= 2;
+        struct pollfd* new_pfds = new struct pollfd[this->_max_c];
+        for (size_t i = 0; i < this->_online_c; ++i)
+            new_pfds[i] = this->_pfds[i];
+        delete[] this->_pfds;
+        this->_pfds = new_pfds;
+    }
+    this->_pfds[this->_online_c].fd = fd;
+    this->_pfds[this->_online_c].events = POLLIN;
+    this->_clients.insert(std::pair<int, Client*>(fd, new Client(fd)));
+    this->_online_c++;
 }
+
 
 void	Server::_removeFromPoll(int fd)
 {
